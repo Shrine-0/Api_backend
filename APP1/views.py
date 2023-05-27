b617425afaa5808c88a55924d6079780eb6e3c4b
Shrine <nischalthapa10@hotmail.com>
2023-05-28 01:15:55 +0545

    made api version of the data pulled

--- APP1/views.py
+++ APP1/views.py
@@ -1,18 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render  
 from django.http import JsonResponse
-
-# import sys 
-# sys.path.append('/OCR_demo')
-
-from OCR_demo import Api_check
+import json 
+import sys 
+sys.path.append('/OCR_demo')
 
 
+f =  open('./OCR_demo/Result.json')
+result = json.load(f)
 
+for i in result['line_items']:
+    print(i)
 def veryfi_api(request):
     
     data={
-        'name':'veryfi'
+        'result':result,
+        'status':'success'
     }
     return JsonResponse(data)
     # return HttpResponse("HEY THIS si working")
--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import OCR_demo
 
 
 def main():
