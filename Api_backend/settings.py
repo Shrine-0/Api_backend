1152f4fcfef295e99397cfdf92cb6641ee01e3cc
Shrine <nischalthapa10@hotmail.com>
2023-05-28 01:27:14 +0545

    made local network accessibilty possible

--- Api_backend/settings.py
+++ Api_backend/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-6otaoej-kpm+oja*iut0z(hd1osn+spz&)09k7bjuu^urao2va
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.1.75',
+    'localhost'
+]
 
 
 # Application definition
